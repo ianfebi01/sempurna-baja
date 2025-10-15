954155766e7b53b871d0ceda73eda1730bbb5e00
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-10-15 14:03:25 +0700

    CHORE: header link

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,10 +1,14 @@
 <template>
   <nav class="main-container flex items-center gap-8 h-16">
-    <NuxtLink to="/" class="no-underline text-black font-extralight">
+    <NuxtLink to="/" exact-active-class="!font-bold" class="no-underline text-black font-extralight transition-default">
       Beranda
     </NuxtLink>
-    <NuxtLink to="/#katalog" class="no-underline text-black font-extralight" @click="scrollToSection('katalog')">
-      Katalog
+    <NuxtLink
+      to="/products"
+      class="no-underline text-black font-extralight transition-default"
+      exact-active-class="!font-bold"
+      @click="scrollToSection('katalog')">
+      Produk
     </NuxtLink>
     <div class="grow"></div>
     <NuxtLink :to="`https://wa.me/6281999980596`" class="button button-primary">
