ca93a6f737069f48c5000b4e05621d86f0c941c3
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-12-15 15:16:53 +0700

    FEAT: add validation to login

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,16 +1,22 @@
 import mongoose from "mongoose"
 import bcrypt from "bcryptjs"
+import z from "zod"
 
 const errorMessage = "Email atau password salah."
 
 export default defineApi( async ( event ) => {
-  // TODO: use validation
   const { email, password } = await readBody( event )
 
-  if ( !email || !password ) {
-    throw createError( {
-      statusMessage: "Email dan password wajib diisi.",
-    } )
+  const schema = z.object( {
+    email    : z.email( "Email tidak valid" ),
+    password : z.string( "Password tidak boleh kosong" ).min( 8, "Password harus terdiri dari minimal 8 karakter" ),
+} )
+
+  const result = schema.safeParse( { email, password } )
+
+  if ( !result.success ) {
+    const first = result.error.issues[0]
+    throw createError( { statusCode: 400, statusMessage: first.message } )
   }
 
   const user = await mongoose.connection.db?.collection( "users" ).findOne( { email } )
