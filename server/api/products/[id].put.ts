d385bed1d19c44dd63c23b058c1e3a27fa6e44d9
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-12-15 14:14:25 +0700

    CHORE: uncomment auth

--- server/api/products/[id].put.ts
+++ server/api/products/[id].put.ts
@@ -45,12 +45,12 @@ export default defineApi( async ( event ) => {
   }
 
   // auth
-  // const me = await requireAuth( event )
-  // const email = me?.email
-  // const user = await UserSchema.findOne( { email } )
-  // if ( !user ) {
-  //   fail( 401, "Tidak diizinkan.", "UNAUTHORIZED" )
-  // }
+  const me = await requireAuth( event )
+  const email = me?.email
+  const user = await UserSchema.findOne( { email } )
+  if ( !user ) {
+    fail( 401, "Tidak diizinkan.", "UNAUTHORIZED" )
+  }
 
   // zod (text fields only)
   const textSchema = z.object( {
