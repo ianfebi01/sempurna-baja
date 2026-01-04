eb12ffe582c9bb73ff3ed5c01d6f74077616dc55
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2026-01-05 00:07:55 +0700

    feat: add privacy policy link to footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
   <footer class="main-container py-8 shadow-sm text-center md:text-left">
     © 2025 Sempurna Baja | Toko & jasa pemasangan baja ringan, galvalum, channal, reng, plafon, dan genteng pasir di Wonosari, Gunungkidul.
+    | <NuxtLink to="/privacy-policy">
+      Privacy Policy
+    </NuxtLink>
   </footer>
 </template>
